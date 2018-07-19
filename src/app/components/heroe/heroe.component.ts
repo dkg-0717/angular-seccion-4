f2da57ba51f51c07950b9ab9035ab3ada1600e38
Angel <dkg.0717@gmail.com>
2018-07-18 20:37:58 -0500

    Rutas terminadas con detalle de heroe

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -8,12 +8,13 @@ import { HeroesService } from '../../servicios/heroes.service';
 })
 export class HeroeComponent {
 
+  heroe:any = {};
+
   constructor(private activatedRoute: ActivatedRoute,
               private heroesService: HeroesService) { 
 
     this.activatedRoute.params.subscribe( params => {
-      console.log(params['id']);
-      heroesService.getHeroe(params['id']);
+      this.heroe = heroesService.getHeroe(params['id']);
     });
   }
 
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -11,16 +11,13 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   constructor( private _heroesService: HeroesService, private router:Router) {
-    console.log('constructor');
    }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
   }
 
   verHeroe(idx:Number) {
-    console.log(idx);
     this.router.navigate(['/heroe', idx]);
   }
 
